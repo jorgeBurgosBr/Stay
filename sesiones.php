974e835b5846c31cb08d56400a32c27e5e4fd8ce
MMJDMM <67686314+MMJDMM@users.noreply.github.com>
2024-02-21 23:05:28 +0100

    Inicio sesiones.php

--- sesiones.php
+++ sesiones.php
@@ -4,11 +4,134 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="./css/style_nav_footer.css">
+   <link rel="stylesheet" href="./css/sesiones_paciente.css">
    <title>Sesiones</title>
 </head>
 
 <body>
-   <h1>Página de sesiones del paciente</h1>
+   <!-- HEADER -->
+   <header>
+      <div class="container-nav">
+         <div class="logo"></div>
+         <nav>
+            <ul>
+               <img src="img/logo.png" alt="Logo" />
+               <li><a href="#">Mi perfil</a></li>
+               <li><a href="#" id="current_page">Sesiones</a></li>
+               <li><a href="#">Mi psicólogo</a></li>
+               <li><a href="#">Artículos</a></li>
+               <li><a href="#">Foro</a></li>
+               <li><a href="#">Talleres</a></li>
+            </ul>
+         </nav>
+      </div>
+   </header>
+   <!-- CALENDARIO -->
+   <div class="container_cal">
+      <div class="celda dias_semana">L</div>
+      <div class="celda dias_semana">M</div>
+      <div class="celda dias_semana">X</div>
+      <div class="celda dias_semana">J</div>
+      <div class="celda dias_semana">V</div>
+      <div class="celda dias_semana">S</div>
+      <div class="celda dias_semana">D</div>
+      <div class="celda pasado_futuro">
+         <span class="num_dia">29</span>
+         <div class="etiqueta_cita">7PM: Sesión con Freud</div>
+      </div>
+      <div class="celda pasado_futuro"><span class="num_dia">30</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">31</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">32</span></div>
+      <div class="celda"><span class="num_dia">1</span></div>
+      <div class="celda"><span class="num_dia">2</span></div>
+      <div class="celda"><span class="num_dia">3</span></div>
+      <div class="celda"><span class="num_dia">4</span></div>
+      <div class="celda"><span class="num_dia">5</span></div>
+      <div class="celda"><span class="num_dia">6</span></div>
+      <div class="celda"><span class="num_dia">7</span></div>
+      <div class="celda"><span class="num_dia">8</span></div>
+      <div class="celda"><span class="num_dia">9</span></div>
+      <div class="celda"><span class="num_dia">10</span></div>
+      <div class="celda"><span class="num_dia">11</span></div>
+      <div class="celda"><span class="num_dia">12</span></div>
+      <div class="celda"><span class="num_dia">13</span></div>
+      <div class="celda"><span class="num_dia">14</span></div>
+      <div class="celda"><span class="num_dia">15</span></div>
+      <div class="celda"><span class="num_dia">16</span></div>
+      <div class="celda"><span class="num_dia">17</span></div>
+      <div class="celda"><span class="num_dia">18</span></div>
+      <div class="celda"><span class="num_dia">19</span></div>
+      <div class="celda"><span class="num_dia">20</span></div>
+      <div class="celda"><span class="num_dia">21</span></div>
+      <div class="celda"><span class="num_dia">22</span></div>
+      <div class="celda"><span class="num_dia">23</span></div>
+      <div class="celda"><span class="num_dia">24</span></div>
+      <div class="celda"><span class="num_dia">25</span></div>
+      <div class="celda"><span class="num_dia">26</span></div>
+      <div class="celda"><span class="num_dia">27</span></div>
+      <div class="celda"><span class="num_dia">28</span></div>
+      <div class="celda"><span class="num_dia">29</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">1</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">2</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">3</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">4</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">5</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">6</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">7</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">8</span></div>
+      <div class="celda pasado_futuro"><span class="num_dia">9</span></div>
+   </div>
+
+   <!-- FOOTER -->
+   <footer>
+      <div class="footer-container">
+         <div class="footer-column">
+            <h3>Stay</h3>
+            <p>Vive con esperanza</p>
+            <p><i class="ri-facebook-fill"></i><i class="ri-twitter-fill"></i><i class="ri-instagram-fill"></i><i class="ri-linkedin-box-fill"></i></p>
+         </div>
+
+         <div class="footer-column">
+            <h3>Quiénes somos</h3>
+            <ul>
+               <li><a href="#">Quiénes somos</a></li>
+               <li><a href="#">Privacidad</a></li>
+               <li><a href="#">Aviso legal</a></li>
+               <li><a href="#">Testimonios</a></li>
+            </ul>
+         </div>
+
+         <div class="footer-column">
+            <h3>Otros</h3>
+            <ul>
+               <li><a href="#">Acerca de</a></li>
+               <li><a href="#">Contacto</a></li>
+               <li><a href="#">Trabaja con nosotros</a></li>
+               <li><a href="#">Talleres</a></li>
+            </ul>
+         </div>
+
+         <div class="footer-column">
+            <h3>Soporte</h3>
+            <ul>
+               <li><a href="#">Chat soporte</a></li>
+               <li><a href="#">Centro de ayuda</a></li>
+               <li><a href="#">Reporta error</a></li>
+               <li><a href="#">Cookies</a></li>
+            </ul>
+         </div>
+
+         <div class="footer-column">
+            <h3>Contáctanos</h3>
+            <ul>
+               <li><i class="ri-mail-line"></i> contacto@stay.com</li>
+               <li><i class="ri-phone-line"></i> 662 223 154</li>
+               <li><i class="ri-map-pin-line"></i> Calle Gran Vía, 77</li>
+            </ul>
+         </div>
+   </footer>
+   <script src="./js/sesiones_paciente.js"></script>
 </body>
 
 </html>
