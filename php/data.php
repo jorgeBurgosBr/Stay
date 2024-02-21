0feb9f2851804058b5d8ddfcea058f6579e95a9c
Pablo <pablonaranjocid@gmail.com>
2024-02-21 12:25:25 +0100

    una tontería

--- php/data.php
+++ php/data.php
@@ -11,7 +11,6 @@ while ($row = mysqli_fetch_assoc($sql)) {
             OR msg_entrada_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
    $query2 = mysqli_query($conn, $sql2);
    $row2 = mysqli_fetch_assoc($query2);
-   $_SESSION['id_cita'] = $row['id_cita'];
    $you = "";
    if (mysqli_num_rows($query2) > 0) {
       $result = $row2['msg'];
