e304c2a7fc1070ee740bb4045d2de2db36efd997
Juan Diego Motta <mottajuandiego@gmail.com>
2024-02-26 15:52:41 +0100

    Avances clase

--- sesiones.php
+++ sesiones.php
@@ -30,78 +30,34 @@
    <div class="container_sesiones">
       <!-- LISTA SESIONES -->
       <div class="container_list_sesiones">
-         <ul id="list_sesiones">
-            <li>Ejemplo de sesion</li>
-            <li>Ejemplo de sesion</li>
-            <li>Ejemplo de sesion</li>
-            <li>Ejemplo de sesion</li>
-         </ul>
-         <span class="material-symbols-outlined" id="icono_chat">
-            chat
-         </span>
+         <div id="listaSesiones">
+            <h2>Próximas sesiones:</h2>
+            <ul id="list_sesiones">
+               <li>Ejemplo de sesion</li>
+               <li>Ejemplo de sesion</li>
+               <li>Ejemplo de sesion</li>
+               <li>Ejemplo de sesion</li>
+            </ul>
+            <span class="material-symbols-outlined" id="icono_chat">
+               chat
+            </span>
+         </div>
       </div>
       <!-- CALENDARIO -->
       <div class="container_cal">
-         <span class="material-symbols-outlined" id="mesAnterior">
-            chevron_left
-         </span>
-         <div id="calendario">
-
+         <div class="container_titulo_cal">
+            <h2 id="titulo_cal"></h2>
+            <span class="material-symbols-outlined" id="mesAnterior">
+               chevron_left
+            </span>
+            <div id="calendario">
+            </div>
+            <span class="material-symbols-outlined" id="mesSiguiente">
+               chevron_right
+            </span>
          </div>
-         <span class="material-symbols-outlined" id="mesSiguiente">
-            chevron_right
-         </span>
       </div>
    </div>
-
-   <!-- FOOTER -->
-   <footer>
-      <div class="footer-container">
-         <div class="footer-column">
-            <h3>Stay</h3>
-            <p>Vive con esperanza</p>
-            <p><i class="ri-facebook-fill"></i><i class="ri-twitter-fill"></i><i class="ri-instagram-fill"></i><i class="ri-linkedin-box-fill"></i></p>
-         </div>
-
-         <div class="footer-column">
-            <h3>Quiénes somos</h3>
-            <ul>
-               <li><a href="#">Quiénes somos</a></li>
-               <li><a href="#">Privacidad</a></li>
-               <li><a href="#">Aviso legal</a></li>
-               <li><a href="#">Testimonios</a></li>
-            </ul>
-         </div>
-
-         <div class="footer-column">
-            <h3>Otros</h3>
-            <ul>
-               <li><a href="#">Acerca de</a></li>
-               <li><a href="#">Contacto</a></li>
-               <li><a href="#">Trabaja con nosotros</a></li>
-               <li><a href="#">Talleres</a></li>
-            </ul>
-         </div>
-
-         <div class="footer-column">
-            <h3>Soporte</h3>
-            <ul>
-               <li><a href="#">Chat soporte</a></li>
-               <li><a href="#">Centro de ayuda</a></li>
-               <li><a href="#">Reporta error</a></li>
-               <li><a href="#">Cookies</a></li>
-            </ul>
-         </div>
-
-         <div class="footer-column">
-            <h3>Contáctanos</h3>
-            <ul>
-               <li><i class="ri-mail-line"></i> contacto@stay.com</li>
-               <li><i class="ri-phone-line"></i> 662 223 154</li>
-               <li><i class="ri-map-pin-line"></i> Calle Gran Vía, 77</li>
-            </ul>
-         </div>
-   </footer>
    <script src="./js/sesiones_paciente.js"></script>
 </body>
 
