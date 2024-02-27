f54e5b6765c33f96da2cb0cecd9a29281d5f996d
Juan Diego Motta <mottajuandiego@gmail.com>
2024-02-27 12:27:38 +0100

    Hamburger

--- sesiones.php
+++ sesiones.php
@@ -6,6 +6,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="./css/style_nav_footer.css">
    <link rel="stylesheet" href="./css/sesiones_paciente.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.1.0/remixicon.css">
    <title>Sesiones</title>
 </head>
 
@@ -14,14 +15,23 @@
    <header>
       <div class="container-nav">
          <div class="logo"></div>
+         <div class="menu-toggle" id="mobile-menu">
+            <img src="img/logo.png" alt="Logo" id="logo-nav"/>
+            <i class="ri-menu-line"></i>
+            </div>
          <nav>
             <ul>
-               <img src="img/logo.png" alt="Logo" />
+               <img src="img/logo.png" alt="Logo" id="logo-menu"/>
                <li><a href="perfil_usuario.php">Mi perfil</a></li>
+               <hr id="separacion">
                <li><a href="#" id="current_page">Sesiones</a></li>
-               <li><a href="#">Mi psicólogo</a></li>
+               <hr id="separacion">
+               <li><a href="#" >Mi psicólogo</a></li>
+               <hr id="separacion">
                <li><a href="#">Artículos</a></li>
+               <hr id="separacion">
                <li><a href="#">Foro</a></li>
+               <hr id="separacion">
                <li><a href="#">Talleres</a></li>
             </ul>
          </nav>
