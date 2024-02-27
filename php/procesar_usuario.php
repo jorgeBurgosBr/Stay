a506f3401c54e79d42b0725c478f5dcec37e6a3d
Jburgos96 <jorge.burgos.barrera@gmail.com>
2024-02-27 20:26:35 +0100

    Añadido apellido en el perfil de usuario

--- php/procesar_usuario.php
+++ php/procesar_usuario.php
@@ -17,7 +17,7 @@ if ($bd->conectar()) {
       $id_paciente = $_SESSION['id_paciente'];
 
       // Realizar la consulta para obtener el nombre y el correo del paciente
-      $sql = mysqli_query($conn, "SELECT p.nombre_paciente, p.correo_paciente, pp.foto_paciente 
+      $sql = mysqli_query($conn, "SELECT p.nombre_paciente, p.apellidos_paciente, p.correo_paciente, pp.foto_paciente 
                             FROM paciente p
                             INNER JOIN perfil_paciente pp ON p.id_paciente = pp.id_paciente
                             WHERE p.id_paciente = '$id_paciente'");
@@ -26,6 +26,7 @@ if ($bd->conectar()) {
       if ($row) {
          $respuesta['success'] = true;
          $respuesta['nombre'] = $row['nombre_paciente'];
+         $respuesta['apellidos'] = $row['apellidos_paciente'];
          $respuesta['correo'] = $row['correo_paciente'];
          $respuesta['photo'] = $row['foto_paciente'];
       } else {
